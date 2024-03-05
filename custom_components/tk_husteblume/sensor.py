f11c2cd104a6812b0382012336f91622090013db
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2024-03-05 11:48:48 +0100

    Initial commit

---
+++ custom_components/tk_husteblume/sensor.py
@@ -0,0 +1,36 @@
+"""Sensor platform for TK Husteblume."""
+from .const import DEFAULT_NAME
+from .const import DOMAIN
+from .const import ICON
+from .const import SENSOR
+from .entity import TkHusteblumeEntity
+
+
+async def async_setup_entry(hass, entry, async_add_devices):
+    """Setup sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_devices([TkHusteblumeSensor(coordinator, entry)])
+
+
+class TkHusteblumeSensor(TkHusteblumeEntity):
+    """tk_husteblume Sensor class."""
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{DEFAULT_NAME}_{SENSOR}"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self.coordinator.data.get("body")
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return ICON
+
+    @property
+    def device_class(self):
+        """Return de device class of the sensor."""
+        return "tk_husteblume__custom_device_class"
